ffcdb8ea531a16dce21a8d3c6948bc71d1bd66a6
Jozef Reda <j0reda97@gmail.com>
2021-12-09 02:15:18 +0200

    second attemp

--- main.js
+++ main.js
@@ -1,29 +1,79 @@
 const yargs = require('yargs')
 
-// Create add command
 yargs.command({
 	command: 'add',
 	describe: 'Adds new student',
 	builder: {
 		id: {
-			demandOption: true, // Required 
+			describe:'student id',
+			demandOption: true, 
 			type: 'number',
-      primaryKey:true
+      		primaryKey:true
 		},
 		name: {
+			describe:'student name',
 			demandOption: true,
 			type: 'string'
 		},
-    grades: {
+	    grades: {
+			describe:'student grades',
 			demandOption: true,
 			type: [int],
 		},
-    comment: {
+  		  comment: {
+			describe:'student comment',
 			type: 'string'
 		},
-    total: {
-      type: 'number'
+		    total: {
+			describe:'student total grades',
+   		    type: 'number'
     }
     
 	},
+	handler:(argv)=>{
+     student.addStudent(argv.id,argv.name,argv.grades,argv.comment,argv.total)
+    }
 })
+
+yargs.command({
+    command:'delete',
+    describe:'Delete student',
+    builder:{
+        id: {
+			describe:'student id',
+			demandOption: true, 
+			type: 'number',
+      		primaryKey:true
+		},
+    },
+    handler:(argv)=>{
+        student.deletestudent(argv.id)
+    }
+})
+ 
+yargs.command({
+    command:'read',
+    describe:'Read student',
+    builder:{
+        id: {
+			describe:'student id',
+			demandOption: true, 
+			type: 'number',
+      		primaryKey:true
+		},
+    },
+    handler:(argv)=>{
+       student.readstudent(argv.id)
+    }
+})
+ 
+yargs.command({
+    command:'list',
+    describe:'List students',
+    handler:()=>{
+        student.listN()
+    }
+})
+
+
+console.log(yargs.argv)
